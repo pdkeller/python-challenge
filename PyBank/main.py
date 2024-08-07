5e5a48ffb3799423855e434c48e21a098a94895f
Paul Keller <paulkeller99@att.net>
2024-08-07 17:24:22 -0500

    PyBank Update

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,7 @@ import os
 import csv
 
 #Create path.
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('python-challenge', 'PyBank', 'Resources', 'budget_data.csv')
 
 #Establish variables.
 month_total = 0
@@ -20,6 +20,8 @@ with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csvheader = next(csvreader)
 
     # Read each row of data after the header
     for row in csvreader:
@@ -27,11 +29,16 @@ with open(csvpath) as csvfile:
         net_total = net_total + int(row[1])
         profit_loss_change.append(int(row[1]))
 
+#Calculations.
+average_change = sum(profit_loss_change) / len(profit_loss_change)
+greatest_increase = max(profit_loss_change)
+greatest_decrease = min(profit_loss_change)
+
 #Print results.
 print("Financial Analysis")
 print("------------------------------------")
-print(f"Total Months: " + month_total)
-print(f"Total: " + net_total)
-print(f"Average Change: " + average_change)
-print(f"Greatest Increase in Profit: " + greatest_increase)
-print(f"Greatest Decrease in Profit: " + greatest_decrease)
+print(f"Total Months: " + str(month_total))
+print(f"Total: $" + str(net_total))
+print(f"Average Change: " + str(average_change))
+print(f"Greatest Increase in Profit: " + str(greatest_increase))
+print(f"Greatest Decrease in Profit: " + str(greatest_decrease))
